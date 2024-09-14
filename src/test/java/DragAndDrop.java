27b83c415aae4596f2bed58b2900754e814e0375
Marina Chen <chen.marina.vm10@gmail.com>
2024-09-14 23:33:34 +0500

    additional task

---
+++ src/test/java/DragAndDrop.java
@@ -0,0 +1,34 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class DragAndDrop {
+    @BeforeAll
+    static void beforeall() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.reopenBrowserOnFail = false;
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void dragAndDropElementTest() {
+        open("/drag_and_drop");
+        $("#column-a").dragAndDrop(to($("#column-b")));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    void dragAndDropElementByActionTest() {
+        open("/drag_and_drop");
+        actions().dragAndDrop($("#column-a"), $("#column-b")).perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
